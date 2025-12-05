647e191e914f078424c7461ce2fd5f33b5c9f2a7
BranProHengker <brannz036@gmail.com>
2025-12-05 10:57:19 +0700

    file sebelum ngerjain feedback

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { RequestService } from '../services/request.service';
 import { User } from '../models/user.model';
 
 @Component({
@@ -19,14 +20,16 @@ export class UserFormComponent implements OnInit {
     password: '',
     companyName: '',
     telp: '',
-    roleId: 2, // Default User
-    roleName: 'User'
+    roleId: undefined, // Will be set when roles are loaded
+    roleName: undefined // Will be set based on selected role
   };
   isEditMode = false;
   isLoading = false;
   errorMsg = '';
+  roles: any[] = []; // Store the roles fetched from the backend
 
   private userService = inject(UserService);
+  private requestService = inject(RequestService);
   private router = inject(Router);
   private route = inject(ActivatedRoute);
 
@@ -34,8 +37,50 @@ export class UserFormComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.isEditMode = true;
-      this.loadUser(Number(id));
+      // Load roles and user together, but ensure roles are loaded first
+      this.loadRolesAndUser(Number(id));
+    } else {
+      // Just load roles for new user creation
+      this.loadRoles();
     }
+  }
+
+  loadRoles() {
+    this.requestService.getRoles().subscribe({
+      next: (data) => {
+        this.roles = Array.isArray(data) ? data : (data as any).content || [];
+        // Set a default role for new user creation if user is not in edit mode
+        if (!this.isEditMode && this.roles.length > 0) {
+          // Set default role (first role in the list as an example, or find 'User' role)
+          const defaultRole = this.roles.find(role => role.roleName === 'User') || this.roles[0];
+          if (defaultRole) {
+            this.user.roleId = defaultRole.id;
+            this.user.roleName = defaultRole.roleName;
+          }
+        }
+      },
+      error: (err) => {
+        console.error('Failed to load roles', err);
+        this.errorMsg = 'Failed to load roles.';
+      }
+    });
+  }
+
+  loadRolesAndUser(userId: number) {
+    // Load roles first, then load user details
+    this.requestService.getRoles().subscribe({
+      next: (data) => {
+        this.roles = Array.isArray(data) ? data : (data as any).content || [];
+        // Now load the user with roles available
+        this.loadUser(userId);
+      },
+      error: (err) => {
+        console.error('Failed to load roles', err);
+        this.errorMsg = 'Failed to load roles.';
+        // Still try to load the user even if roles failed
+        this.loadUser(userId);
+      }
+    });
   }
 
   loadUser(id: number) {
@@ -45,7 +90,12 @@ export class UserFormComponent implements OnInit {
         if (found) {
           this.user = { ...found };
           // Clear password for edit mode security
-          this.user.password = ''; 
+          this.user.password = '';
+
+          // If roles are already loaded, update the roleId/roleName
+          if (this.roles.length > 0) {
+            this.updateUserRoleFromRoles(found);
+          }
         }
         this.isLoading = false;
       },
@@ -57,11 +107,33 @@ export class UserFormComponent implements OnInit {
     });
   }
 
+  updateUserRoleFromRoles(found: User) {
+    const matchedRole = this.roles.find(role => role.roleName === found.roleName || role.id === found.roleId);
+    if (matchedRole) {
+      this.user.roleId = matchedRole.id;
+      this.user.roleName = matchedRole.roleName;
+    }
+  }
+
+  updateRoleName(roleId: number) {
+    const selectedRole = this.roles.find(role => role.id === roleId);
+    if (selectedRole) {
+      this.user.roleName = selectedRole.roleName;
+    }
+  }
+
   onSubmit() {
     this.isLoading = true;
     this.errorMsg = '';
 
-    const request = this.isEditMode 
+    // Make sure we have a roleId selected
+    if (this.user.roleId === undefined || this.user.roleId === null) {
+      this.errorMsg = 'Please select a role.';
+      this.isLoading = false;
+      return;
+    }
+
+    const request = this.isEditMode
       ? this.userService.updateUser(this.user)
       : this.userService.createUser(this.user);
 
